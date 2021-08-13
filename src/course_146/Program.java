f4687770e781c78464e8afb814583b1a153756e2
Dante Galiotto <dantegaliotto@gmail.com>
2021-08-13 04:48:52 -0600

    Projeto criado

---
+++ src/course_146/Program.java
@@ -0,0 +1,50 @@
+package course_146;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainException;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			
+			System.out.println("Enter account data");
+			System.out.print("Number: ");
+			int number = sc.nextInt();
+			sc.nextLine();
+			System.out.print("Holder: ");
+			String holder = sc.nextLine();
+			System.out.print("Initial balance: ");
+			double initialBalance = sc.nextDouble();
+			System.out.print("Withdraw limit: ");
+			double withdrawLimit = sc.nextDouble();
+			
+			Account account = new Account(number, holder, initialBalance, withdrawLimit);
+			
+			System.out.println("");
+			System.out.print("Enter amount for withdraw: ");
+			double amountWithdraw = sc.nextDouble();
+			
+			account.withdraw(amountWithdraw);
+			
+			System.out.println("New balance: " + String.format("%.2f",account.getBalance()));
+		}
+		catch (DomainException e) {
+			System.out.println("Withdraw error: " + e.getMessage());
+		}
+		catch (Exception e) {
+			System.out.println("Generic exception: " + e.getMessage());
+		}
+		
+		sc.close();
+
+	}
+
+}
